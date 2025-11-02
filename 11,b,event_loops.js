937f9583fc86923efed0be468fab57d72e7b743c
JRM1-star <177595178+JRM1-star@users.noreply.github.com>
2025-11-02 21:24:06 +0530

    Add files via upload

---
+++ 11,b,event_loops.js
@@ -0,0 +1,10 @@
+setTimeout(function timer()
+{
+console.log('2 seconds over, You clicked the button!');
+}, 2000);
+console.log("hi!");
+setTimeout(function timeout()
+{
+console.log('5 seconds over, You clicked the button!');
+}, 5000);
+console.log("Welcome to event loops");
